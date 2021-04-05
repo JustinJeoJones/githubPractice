bb0084d1b5462cb3d525d7791f5a94fdc816d6bd
Jeo <jeo961@gmail.com>
2021-04-05 10:36:02 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ageF = int.Parse(age);
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine(name +" "+ ageF + " " + color);
+        }
+    }
+}
